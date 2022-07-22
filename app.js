d18085f74cd0e8d9c1fc881f40f5c951c8f8c7c5
hiimteddie <ethanlian18@gmail.com>
2022-07-22 18:59:23 +0800

    add

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
-// template_jbmhcfs
-// service_6zd1d0o
-// IwZ9dkwbz5Ck5hYE2
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20;
 
-function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList += " modal__overlay--visible"
-    emailjs
-      .sendForm(
-        'service_6zd1d0o',
-        'template_jbmhcfs',
-        event.target,
-        'IwZ9dkwbz5Ck5hYE2'
-    ).then(() => {
-        loading.classList.remove("modal__overlay--visible");
-        success.classList += " modal__overlay--visible";
-    }).catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-            "The email service is temporarily unavailable. Please contact me directly at ethanlian18@gmail.com"
-        );
-    })    
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape")
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
 }
 
-let isModalOpen = false;
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_6zd1d0o",
+      "template_jbmhcfs",
+      event.target,
+      "IwZ9dkwbz5Ck5hYE2"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at ethanlian18@gmail.com"
+      );
+    });
+}
+
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open"
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
